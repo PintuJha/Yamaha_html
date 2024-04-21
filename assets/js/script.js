a213da594c7176af866a71de07abf32cee7a5a78
P_jha <pintu@ascratech.com>
2024-04-22 00:14:46 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,111 @@ $(function ($) {
     });
   }
 });
+
+
+
+// =============Product Slider Code======================
+$(document).ready(function() {
+  var owl = $("#recent_event");
+  owl.owlCarousel({
+  autoPlay: 3000,
+  items : 2, //10 items above 1000px browser width
+  itemsDesktop : [1000,4], //5 items between 1000px and 901px
+  itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+  itemsTablet: [600,2], //2 items between 600 and 0;
+  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  pagination:false
+  });
+  $(".next").click(function(){
+      owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+      owl.trigger('owl.prev');
+  })
+});
+
+
+$(document).ready(function() {
+  var owl = $("#recent_event1");
+  owl.owlCarousel({
+  autoPlay: 3000,
+  items : 2, //10 items above 1000px browser width
+  itemsDesktop : [1000,4], //5 items between 1000px and 901px
+  itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+  itemsTablet: [600,2], //2 items between 600 and 0;
+  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  pagination:false
+  });
+  $(".next").click(function(){
+      owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+      owl.trigger('owl.prev');
+  })
+});
+
+
+$(document).ready(function() {
+  var owl = $("#recent_event2");
+  owl.owlCarousel({
+  autoPlay: 3000,
+  items : 2, //10 items above 1000px browser width
+  itemsDesktop : [1000,4], //5 items between 1000px and 901px
+  itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+  itemsTablet: [600,2], //2 items between 600 and 0;
+  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  pagination:false
+  });
+  $(".next").click(function(){
+      owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+      owl.trigger('owl.prev');
+  })
+});
+
+
+
+
+$(document).ready(function() {
+  var owl = $("#recent_event11");
+  owl.owlCarousel({
+  autoPlay: 4000,
+  items : 2, //10 items above 1000px browser width
+  itemsDesktop : [1000,4], //5 items between 1000px and 901px
+  itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+  itemsTablet: [600,2], //2 items between 600 and 0;
+  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  pagination:false
+  });
+  $(".next").click(function(){
+      owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+      owl.trigger('owl.prev');
+  })
+});
+
+
+$(document).ready(function() {
+  var owl = $("#recent_event22");
+  owl.owlCarousel({
+  autoPlay: 4000,
+  items : 2, //10 items above 1000px browser width
+  itemsDesktop : [1000,4], //5 items between 1000px and 901px
+  itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+  itemsTablet: [600,2], //2 items between 600 and 0;
+  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+  pagination:false
+  });
+  $(".next").click(function(){
+      owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+      owl.trigger('owl.prev');
+  })
+});
+
+
+
+// ==============Search code start==================
